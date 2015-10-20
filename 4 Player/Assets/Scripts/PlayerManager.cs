c7b57f0f0c4533f50ebd645358c4c67ede26e054
LaserAce <s2-landy@STU-CZC3280QD7.campus.ads.uwe.ac.uk>
2015-10-20 13:10:35 +0100

    4 Player Template
    Basic player movement
    4 player support
    can instantiate and destroy players using start and back

---
+++ 4 Player/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour 
+{
+	private const int MAX_PLAYERS = 4;
+	private bool[] activeControllers;
+	private GameObject[] players;
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Setting up initial arrays
+		activeControllers = new bool[MAX_PLAYERS];
+		players = new GameObject[MAX_PLAYERS];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Loop for through the maximum possible players
+		for (int i = 0; i < MAX_PLAYERS; ++i) 
+		{
+			//If a player presses start then spawn player and set id
+			if (Input.GetButton ("Start" + (i + 1))) 
+			{
+				if(activeControllers[i] == false)
+				{
+					players[i] = Instantiate(playerPrefab, new Vector3(i*2, 0, i*2), Quaternion.identity) as GameObject;
+					players[i].GetComponent<PlayerMovement>().SetupPlayerID(i+1);
+					activeControllers[i] = true;
+				}
+			}
+			//Else if player presses back then destroy player
+			else if (Input.GetButton ("Back" + (i + 1))) 
+			{
+				if(activeControllers[i] == true)
+				{
+					Destroy(players[i]);
+					activeControllers[i] = false;
+				}
+			}
+		}
+	}
+}
---
+++ 4 Player/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float movementSpeed = 5;
+	private int playerID = 1;
+
+	//private bool playerActive = false;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if(playerActive == true)
+		//{
+		//Basic movement
+			float moveHorizontal = Input.GetAxis ("Horizontal" + playerID);
+			float moveVertical = Input.GetAxis ("Vertical" + playerID);
+			
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			transform.position += (movement * Time.deltaTime * movementSpeed);
+		//}
+	
+	}
+
+	public void SetupPlayerID(int ID)
+	{
+		playerID = ID;
+	}
+}
