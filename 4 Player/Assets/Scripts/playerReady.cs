178e768073e34c3a9e33aee9755715514ebea321
hopptramp <rob-hopps@hotmail.co.uk>
2015-10-20 13:24:54 +0100

    - start of Menu - start of split screen

---
+++ 4 Player/Assets/Scripts/playerReady.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerReady : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Get references to players
+		GameObject player1 = GameObject.Find ("Player1");
+		GameObject player2 = GameObject.Find ("Player2");
+		GameObject player3 = GameObject.Find ("Player3");
+		GameObject player4 = GameObject.Find ("Player4");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// if player1 presses start
+			// change playerOneStart text to say playerOneReady
+			// add player1 to list/array with players
+			// set bool for player1 in true
+
+		// if player1 is in (bool)
+		//  	if player1 presses back
+		//			change text back to playerOneStart
+		//			remove player1 from list/array
+		//			set bool for player1 in false
+
+		// repeat for each player
+
+		// if array.length >= 2 
+		//		make start button visible
+		//
+	}
+}
