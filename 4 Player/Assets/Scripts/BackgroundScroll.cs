f12205e20c660f5ac6284225c26e924fe73a297b
xavboy <aldrodgex@gmail.com>
2015-10-30 20:35:31 +0000

    Added in a background and scrolling background texture
    Prototype background and background and scrolling idea. Working on
    player sprites.  Tried to incorporate dark theme.

---
+++ 4 Player/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+
+
+
+	public Transform start;
+	//public Transform posObj;
+	public Transform end;
+
+	public float speed = 10.0f;
+	Transform destination;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(transform.position.y < end.position.y)
+		{
+			transform.Translate(0,start.position.y,0);
+		}
+
+		transform.Translate(0,-speed *Time.deltaTime,0);
+}
+}
