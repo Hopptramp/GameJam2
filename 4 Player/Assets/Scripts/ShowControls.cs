6413479a8b756300497f1786090c9a52fd5e2a9a
Ed-McD <edard_666@hotmail.co.uk>
2015-11-02 12:49:10 +0000

    Stuff for showing contols
    Y on controller, left shift on keyboard switches to contol layout in
    menu

---
+++ 4 Player/Assets/Scripts/ShowControls.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowControls : MonoBehaviour {
+
+
+    
+    public bool showControls = false;
+    public bool canvasHidden = false;
+    public GameObject canvas;
+    public GameObject controlPlane;
+
+	// Use this for initialization
+	void Start ()
+    {
+    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (showControls)
+        {
+            if (!canvasHidden)
+            {
+                canvas.SetActive(false);
+                controlPlane.SetActive(true);
+                canvasHidden = true;
+                
+
+            }
+            else
+            {
+                if (Input.GetButtonDown("Controls"))
+                {
+                    Debug.Log("Input Happened!");
+                    canvas.SetActive(true);
+                    controlPlane.SetActive(false);
+                    canvasHidden = false;
+                    showControls = false;
+                    
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Controls"))
+            {
+                Debug.Log("Input Happened!");
+                showControls = true;
+
+            }
+        }
+	
+	}
+}
